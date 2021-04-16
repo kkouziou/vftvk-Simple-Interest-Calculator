9037ebf361527f6626cebff929a4b45f222aa4da
Konstantinos Kouziou <k.kouziou@gmail.com>
2021-04-16 13:20:22 +0000

    code without comments

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
+{   
+    if (checkdata()== false){
+        return
+    }
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    const result = 'If you deposit <mark>' + principal + '</mark>, <br>' + 
+    'at an interest rate of <mark>' + rate +'%</mark>. <br>' +
+    'You will receive an amount of <mark>' + interest +'</mark>, <br>' +
+    'in the year <mark>' + year +'</mark>' ;
+    document.getElementById("result").innerHTML=result ;
+}
+
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value +'%';
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function checkdata(){
+    var principal = document.getElementById("principal");
+    if (principal.value<=0){
+        alert('Enter a positive number');
+        principal.focus();
+        return false;
+    }
+    return true;
 }
         
